65ad00261f707a38904f987324b7e23a70313028
Adam Hymus <hymusprint@live.com>
2024-03-16 02:16:33 +0000

    Update

--- components/heroBlock.js
+++ components/heroBlock.js
@@ -44,11 +44,11 @@ export default function HeaderBar() {
         </div>
         <div className="max-w-6xl mb-12 w-full lg:w-4/5">
           <p
-            className={`text-yellow-300 text-4xl mb-2 text-center lg:text-start ${Shadows_Into_Light1.className}`}
+            className={`text-yellow-300 text-2xl sm:text-4xl mb-2 text-center lg:text-start ${Shadows_Into_Light1.className}`}
           >
             Commerical & Domestic
           </p>
-          <p className="text-[70px] sm:text-[90px] mb-6 leading-[60px] sm:leading-[75px] font-semibold text-center lg:text-start ">
+          <p className="text-[55px] sm:text-[90px] mb-6 leading-[60px] sm:leading-[75px] font-semibold text-center lg:text-start ">
             Professional Cleaning Specialists.
           </p>
           <p className="text-md  mb-6 leading-5 text-center lg:text-start">
@@ -62,7 +62,7 @@ export default function HeaderBar() {
               <a
                 onClick={scrollToPlans}
                 title="Get quote now"
-                class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-slate-900 font-pj rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                class="relative inline-flex items-center justify-center px-5 py-3 sm:px-8 sm:py-4 text-lg font-bold text-white transition-all duration-200 bg-slate-900 font-pj rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                 role="button"
               >
                 Our Services
@@ -72,7 +72,7 @@ export default function HeaderBar() {
               <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#89beff] via-[#44f6ff] to-[#5eefff] rounded-md blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
               <a
                 title="Get quote now"
-                class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-slate-900 font-pj rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                class="relative inline-flex items-center justify-center px-5 py-3 sm:px-8 sm:py-4 text-lg font-bold text-white transition-all duration-200 bg-slate-900 font-pj rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                 role="button"
               >
                 Contact Us
